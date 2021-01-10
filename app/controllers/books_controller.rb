054478db2531f75b01da3a4216261023c810dc36
zuotengyui1@gmail.com <ec2-user@ip-172-31-41-66.ec2.internal>
2021-01-10 12:44:14 +0000

    提出課題です

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_current_user, {only: [:edit,:update,:destroy]}
+
+  def create
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+     if @book.save
+       redirect_to books_path
+     else
+     end
+  end
+
+  def index
+      @books = Book.all
+      @user = current_user
+      @book = Book.new
+  end
+
+  def show
+      @book = Book.find(params[:id])
+      @user = @book.user # User.find(@book.user_id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:destroy] = 'Book was successfully destroyed.'
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = 'Book was successfully created.'
+    redirect_to book_path(@book.id)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
+  end
+
+  def  ensure_current_user
+      @book = Book.find(params[:id])
+     if @book.user_id != current_user.id
+        redirect_to books_path
+     end
+  end
+end
